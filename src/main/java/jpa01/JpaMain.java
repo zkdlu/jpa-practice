83f4327a1489e46342d94fd44f57c3b853ab728e
zkdlu <zkdlu159951@gmail.com>
2022-02-17 20:15:20 +0900

    entity manager transaction

--- src/main/java/jpa01/JpaMain.java
+++ src/main/java/jpa01/JpaMain.java
@@ -2,14 +2,30 @@ package jpa01;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello-jpa");
-
         EntityManager em = emf.createEntityManager();
-        em.close();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("lee");
+
+            em.persist(member);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
 
         emf.close();;
     }
