7e209e6b5dbc1859edca12eb4dba574b4ea6afa2
zkdlu <zkdlu159951@gmail.com>
2022-02-17 20:10:00 +0900

    jpa init

---
+++ src/main/java/jpa01/JpaMain.java
@@ -0,0 +1,16 @@
+package jpa01;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello-jpa");
+
+        EntityManager em = emf.createEntityManager();
+        em.close();
+
+        emf.close();;
+    }
+}
