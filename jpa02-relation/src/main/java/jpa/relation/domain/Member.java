3172e2d600e06aa827e323630a321e1ffe5b86cf
zkdlu <zkdlu159951@gmail.com>
2022-02-21 20:29:03 +0900

    jpa 단방향 연관관계

---
+++ jpa02-relation/src/main/java/jpa/relation/domain/Member.java
@@ -0,0 +1,26 @@
+package jpa.relation.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Getter
+@Setter
+@Entity
+public class Member {
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+    @Column(name = "NAME")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+}
