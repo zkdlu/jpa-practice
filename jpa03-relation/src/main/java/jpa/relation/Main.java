c21986d8c796bac22e852b120d0a28ef012d874f
zkdlu <zkdlu159951@gmail.com>
2022-02-21 20:33:27 +0900

    jpa 양방향 연관관계

---
+++ jpa03-relation/src/main/java/jpa/relation/Main.java
@@ -0,0 +1,51 @@
+package jpa.relation;
+
+import jpa.relation.domain.Member;
+import jpa.relation.domain.Team;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello-jpa");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Team team = new Team();
+            team.setName("team 1");
+            em.persist(team);
+
+            System.out.println("==========");
+
+            Member member = new Member();
+            member.setName("건");
+            member.setTeam(team);
+            em.persist(member);
+
+            em.flush(); // 영속성 컨텍스트에 있는걸 db에 쿼리를 날려 싱크 반영
+            em.clear(); // 영속성 컨텍스트 초기화
+
+            Member findMember = em.find(Member.class, member.getId());
+            List<Member> members = findMember.getTeam().getMembers();
+
+            for (Member m : members) {
+                System.out.println("m = " + m.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();;
+    }
+}
---
+++ jpa03-relation/src/main/java/jpa/relation/domain/Team.java
@@ -0,0 +1,25 @@
+package jpa.relation.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter @Setter
+@Entity
+public class Team {
+    @Id @GeneratedValue
+    @Column(name = "TEAM_ID")
+    private Long id;
+    @Column(name = "NAME")
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+}
