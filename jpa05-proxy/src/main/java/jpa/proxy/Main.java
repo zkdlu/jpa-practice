f697ec57789c9912a3b0f288b9b6027f78a489f6
zkdlu <zkdlu159951@gmail.com>
2022-02-23 20:46:25 +0900

    hibernate proxy object

--- jpa05-proxy/src/main/java/jpa/proxy/Main.java
+++ jpa05-proxy/src/main/java/jpa/proxy/Main.java
@@ -20,11 +20,30 @@ public class Main {
             member.setName("hello");
             em.persist(member);
 
+            Member member2 = new Member();
+            member2.setName("hello");
+            em.persist(member2);
+
             em.flush();
             em.clear();
 
-            Member findMember = em.getReference(Member.class, member.getId());
+            Member m1 = em.find(Member.class, member.getId());
+            Member m2 = em.getReference(Member.class, member2.getId());
+
+            System.out.println("m1 -> " + m1.getName());
+            System.out.println("m2 -> " + m2.getName());
+
+            System.out.println("m1 == m2 : " + (m1 == m2));
+            System.out.println("m1 : " + (m1 instanceof Member));
+            System.out.println("m2  : " + (m2 instanceof Member));
+
+            Member refMember = em.getReference(Member.class, member.getId());
+            System.out.println("refMember = " + refMember.getClass());
+
+            Member findMember = em.find(Member.class, member.getId());
             System.out.println("findMember = " + findMember.getClass());
+
+            System.out.println("ref == find : " + (refMember == findMember));
 
             tx.commit();
         } catch (Exception e) {
