37eaf1b6af554e166d2d15cc270bd8b0071869b6
zkdlu <zkdlu159951@gmail.com>
2022-02-26 01:12:04 +0900

    값 타입의 동등성(equals)보장

--- jpa06-valueType/src/main/java/jpa/valueType/domain/Address.java
+++ jpa06-valueType/src/main/java/jpa/valueType/domain/Address.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
 @NoArgsConstructor
 @Getter @Setter
@@ -18,5 +19,18 @@ public class Address {
         this.city = city;
         this.street = street;
         this.zip = zip;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(city, address.city) && Objects.equals(street, address.street) && Objects.equals(zip, address.zip);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, zip);
     }
 }
