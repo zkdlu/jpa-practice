6f704e4c91a23d6c974b1ddfbca46fba98e02c98
zkdlu <zkdlu159951@gmail.com>
2022-02-26 00:57:14 +0900

    값 타입 오버라이딩

--- jpa06-valueType/src/main/java/jpa/valueType/domain/Member.java
+++ jpa06-valueType/src/main/java/jpa/valueType/domain/Member.java
@@ -3,6 +3,9 @@ package jpa.valueType.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -28,4 +31,13 @@ public class Member {
     private Period period;
     @Embedded
     private Address address;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "city", column = @Column(name = "work_city")),
+            @AttributeOverride(name = "street", column = @Column(name = "work_street")),
+            @AttributeOverride(name = "zip", column = @Column(name = "work_zip"))
+
+    })
+    private Address workAddress;
 }
